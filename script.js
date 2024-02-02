99de1ba6bcb6caab0d9396b94c31e30b11bbd2c6
SilkeSavanna <sihe0002@stud.kea.dk>
2024-02-02 10:14:51 +0100

    template added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Let go!");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+
+function init() {
+  //starter med at hente dataen fra URL & then bruges til at kalde en funktion når dataten er hentet. Herefter laver man responset om til JSON//
+  fetch(beerURL)
+    .then(function (response) {
+      return response.json();
+    })
+    //
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
