0b66e9c801c5759acb47f25a290508af793e82d0
SilkeSavanna <sihe0002@stud.kea.dk>
2024-02-02 11:04:49 +0100

    a card added dynamically

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ window.addEventListener("DOMContentLoaded", init);
 
 const beerURL = "https://api.punkapi.com/v2/beers";
 
+let beerTemplate;
+let beerContainer;
+
 function init() {
+  console.log("init");
+
+  beerTemplate = document.querySelector(".beer_template");
+  console.log("beer_template", beerTemplate);
+
+  beerContainer = document.querySelector(".beer_container");
+  console.log("beer_container", beerContainer);
+
   //starter med at hente dataen fra URL & then bruges til at kalde en funktion når dataten er hentet. Herefter laver man responset om til JSON//
   fetch(beerURL)
     .then(function (response) {
@@ -10,6 +21,22 @@ function init() {
     })
     //
     .then(function (json) {
-      console.log("json", json);
+      showBeers(json);
     });
+}
+
+function showBeers(beerJSON) {
+  console.log("Første json element", beerJSON[0]);
+
+  let beerClone = beerTemplate.cloneNode(true).content;
+  beerClone.querySelector(".beer_image").src = beerJSON[0].image_url;
+  beerClone.querySelector(
+    ".beer_image"
+  ).alt = `picture of a ${beerJSON[0].name} beer`;
+  beerClone.querySelector(".beer_name").textContent = beerJSON[0].name;
+  beerClone.querySelector(".beer_tagline").textContent = beerJSON[0].tagline;
+  beerClone.querySelector(".beer_description").textContent =
+    beerJSON[0].description;
+
+  beerContainer.appendChild(beerClone);
 }
